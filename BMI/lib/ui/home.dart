17b661831aed670a0fefbc0dae1b018150dfefe5
Chathmini <chathmini96@gmail.com>
2020-08-19 23:42:27 +0530

    add logic part

--- BMI/lib/ui/home.dart
+++ BMI/lib/ui/home.dart
@@ -8,7 +8,46 @@ class Bmi extends StatefulWidget {
 }
 
 class BmiState extends State<Bmi> {
-  get height => null;
+  final TextEditingController _ageController = new TextEditingController();
+  final TextEditingController _heightController = new TextEditingController();
+  final TextEditingController _weightController = new TextEditingController();
+  double inches = 0.0;
+  double result = 0.0;
+  String _resultReading = "";
+  String _finalResult = "";
+
+  void _calculateBMI() {
+    setState(() {
+      int age = int.parse(_ageController.text);
+      double height = double.parse(_heightController.text);
+      inches = height * 12;
+      double weight = double.parse(_weightController.text);
+      if ((_ageController.text.isNotEmpty || age > 0) &&
+          (_heightController.text.isNotEmpty || inches > 0) &&
+          (_weightController.text.isNotEmpty && weight > 0)) {
+        result = weight / (inches * inches) * 703;
+        if (double.parse(result.toStringAsFixed(1)) < 18.5) {
+          _resultReading = "UnderWeight";
+          print(_resultReading);
+        } else if (double.parse(result.toStringAsFixed(1)) >= 18.5 &&
+            result < 25) {
+          _resultReading = "Great Shape!";
+          print(_resultReading);
+        } else if (double.parse(result.toStringAsFixed(1)) > 25.0 &&
+            result < 30) {
+          _resultReading = "Overweight";
+          print(_resultReading);
+        } else if (double.parse(result.toStringAsFixed(1)) >= 30) {
+          _resultReading = "Obese";
+          print(_resultReading);
+        }
+      } else {
+        result = 0.0;
+      }
+    });
+
+    _finalResult = "Your BMI: ${result.toStringAsExponential(1)}  ";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +75,7 @@ class BmiState extends State<Bmi> {
                 child: new Column(
                   children: <Widget>[
                     new TextField(
-                      controller: null,
+                      controller: _ageController,
                       keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                         labelText: 'Age',
@@ -45,7 +84,7 @@ class BmiState extends State<Bmi> {
                       ),
                     ),
                     new TextField(
-                      controller: null,
+                      controller: _heightController,
                       keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                         labelText: 'Height in feet',
@@ -54,7 +93,7 @@ class BmiState extends State<Bmi> {
                       ),
                     ),
                     new TextField(
-                      controller: null,
+                      controller: _weightController,
                       keyboardType: TextInputType.number,
                       decoration: new InputDecoration(
                         labelText: 'Weight in lbs',
@@ -66,7 +105,7 @@ class BmiState extends State<Bmi> {
                     new Container(
                       alignment: Alignment.center,
                       child: new RaisedButton(
-                        onPressed: () => debugPrint("Hell"),
+                        onPressed: _calculateBMI,
                         color: Colors.pinkAccent,
                         child: new Text('Claculate'),
                         textColor: Colors.white,
@@ -79,7 +118,7 @@ class BmiState extends State<Bmi> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 new Text(
-                  'BMI',
+                  'BMI: $_finalResult',
                   style: new TextStyle(
                       color: Colors.blueAccent,
                       fontWeight: FontWeight.w600,
@@ -88,7 +127,7 @@ class BmiState extends State<Bmi> {
                 ),
                 new Padding(padding: const EdgeInsets.all(5.0)),
                 new Text(
-                  'Overweight',
+                  "$_resultReading",
                   style: new TextStyle(
                       color: Colors.pinkAccent,
                       fontWeight: FontWeight.w400,
